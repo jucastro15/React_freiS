0b94f84e4b6f9d7dedf7d894784c1b41116ef698
Júlia de Castro Bueno <ra56421610838@cursos.insf>
2024-09-09 17:12:02 -0300

    Foi

--- src/pages/exercicios/exe8/index.jsx
+++ src/pages/exercicios/exe8/index.jsx
@@ -1,39 +1,61 @@
 import './index.scss'
 import Cabecalho from '../../../components/cabecalho'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useState} from 'react'
 
 export default function Temperatura() {
 
     const [temperatura, setTemperatura] = useState(0)
     const [situacao, setSituacao] = useState(0)
+    const [temperatura2, setTemperatura2] = useState(false)
+
+
+
 
 
     function converter() {
 
+     
+
         if (temperatura >= 41) {
             setSituacao(`Hipertermia`)
+            setTemperatura2(true)
         }
 
         else if (temperatura >= 39.6 && temperatura < 41) {
             setSituacao(`Febre Alta`)
+            setTemperatura2(true)
         }
 
         else if (temperatura >= 37.6 && temperatura < 39.6) {
             setSituacao(`Febre`)
+            setTemperatura2(true)
         }
 
         else if (temperatura >= 36 && temperatura < 37.6) {
             setSituacao(`Normal`)
+            setTemperatura2(false)
         }
 
-        else {
+        else  {
             setSituacao(`Hipotermia`)
+            setTemperatura2(false)
         }
+       
 
+       
 
+    
+        setTemperatura('')
+       
 
     }
+
+
+
+
+
+
 
     return (
         <div className='pagina-ex8 pagina'>
@@ -57,18 +79,17 @@ export default function Temperatura() {
 
 
 
-            <section className='ex'>
+                <section  className='ex'>
                 <div className='tempes'>
                     <img src="/assets/images/temperatura.webp" alt="" />
                 </div>
-                <div className='card'>
-                    <h2>Qual a sua temperatura</h2>
+                <div style={{ backgroundColor: temperatura2 === true   ? "#c52f2f" : '#fff' }} className='card'>
+                    <h2  style={{ color: temperatura2 === true ? '#fff' : '#000' }}>Qual a sua temperatura</h2>
                     <input type="text" value={temperatura} onChange={e => setTemperatura(e.target.value)} />
 
                     <div>
                         <button onClick={converter} >Executar</button>
                     </div>
-
 
                 </div>
                 <h3>Situacao para a sua temperatura é {situacao} </h3>
